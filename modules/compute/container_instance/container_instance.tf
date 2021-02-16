4d43b1b58b1a43b49d6dee3c50d9694f86d8c0cc
Ben Hummerstone <bhummerstone@users.noreply.github.com>
2021-02-16 11:56:46 +0000

    adding complexity

--- modules/compute/container_instance/container_instance.tf
+++ modules/compute/container_instance/container_instance.tf
@@ -12,9 +12,10 @@ resource "azurerm_container_group" "aci" {
   name = azurecaf_name.aci.result
   resource_group_name = var.resource_group_name
   location = var.location
-  ip_address_type = var.ip_address_type // optional, Public or Private
-  dns_name_label = var.dns_name_label // optional, not supported if using vNet
-  os_type = var.os_type // required, if Windows, only 1 container block
+  ip_address_type = try(var.ip_address_type, null)
+  network_profile_id = try(var.network_profile_id, null)
+  dns_name_label = try(var.dns_name_label, null)
+  os_type = var.os_type
 
   dynamic "identity" {
     for_each = try(var.identity, {})
@@ -32,10 +33,28 @@ resource "azurerm_container_group" "aci" {
     }
   }
   
+  dynamic "network_profile_id" {
+
+  }
   //1 or more container block
   //network_profile_id if ip type set to Private
   //image_registry_credential
   //restart_policy
 
   
+}
+
+resource "azurerm_network_profile" "aci" {
+  name = azurecaf_name.anp.result
+  resource_group_name = var.resource_group_name
+  location = var.location
+
+  container_network_interface {
+    name = "${azurecaf_name.aci.result}-nic"
+
+    ip_configuration {
+      name = "${azurecaf_name.aci.result}-ipconfig"
+      subnet_id = try(var.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, var.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
+    }
+  }
 }
